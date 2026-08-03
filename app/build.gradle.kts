23fa98713b7e1e32c5eb156d8cb8a559cae84d1e
khj1870-robotics <a01047435935@gmail.com>
2026-08-03 18:07:29 +0900

    업데이트 설치용 앱 버전 증가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.exhibitionarchive"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
